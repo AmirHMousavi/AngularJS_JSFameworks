886ecc188f8ababb67c035a53d08b7e99d48f0a7
AmirHMousavi <ah.mousavi@hotmail.com>
2016-08-09 05:09:13 +0200

    From&Validation
    adding contact us page + feedback form and regarding form validation
    with angular (not HTML validate)

--- conFusion/app/scripts/app.js
+++ conFusion/app/scripts/app.js
@@ -73,4 +73,33 @@ $scope.toggleDetails = function() {
   $scope.isSelected = function (checkTab) {
     return ($scope.tab === checkTab);
   };
+}])
+
+.controller('ContactController', ['$scope', function($scope) {
+
+    $scope.feedback = {mychannel:"", firstName:"", lastName:"",
+                               agree:false, email:"" };
+            var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
+    $scope.channels = channels;
+    $scope.invalidChannelSelection = false;
+}])
+
+.controller('FeedbackController', ['$scope', function($scope) {
+     $scope.sendFeedback = function() {
+                console.log($scope.feedback);
+                if ($scope.feedback.agree && ($scope.feedback.mychannel == "")&& !$scope.feedback.mychannel){
+                    $scope.invalidChannelSelection = true;
+                    console.log('incorrect');
+                }
+                else {
+                    $scope.invalidChannelSelection = false;
+                    $scope.feedback = {mychannel:"", firstName:"", lastName:"",
+                                       agree:false, email:"" };
+                    $scope.feedback.mychannel="";
+
+                    $scope.feedbackForm.$setPristine();
+                    console.log($scope.feedback);
+                }
+     };
+         
 }]);
